6b582324e40394fd09d825ba406dd0fa67d6f12f
Conor Maguire <conormag94@gmail.com>
2018-03-24 14:39:09 +0000

    Map can now be rotated using on screen buttons

---
+++ Assets/MapController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Unity.Map;
+using Mapbox.Utils;
+
+public class MapController : MonoBehaviour {
+
+	private Quaternion targetRotation;
+	private float rotateSpeed = 60.0f;
+	private float rotateAmount = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+		targetRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.RotateTowards (transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+	}
+
+	public void RotateClockwise () {
+		targetRotation = Quaternion.AngleAxis (rotateAmount, Vector3.up) * targetRotation; 
+	}
+
+	public void RotateAntiClockwise () {
+		targetRotation = Quaternion.AngleAxis (-rotateAmount, Vector3.up) * targetRotation;
+	}
+}
