64ee7b1f0e67524e7370b6326567f62b9122ef49
DarraghMc77 <mccaffrd@tcd.ie>
2018-04-20 13:54:51 +0100

    Added endpoint for getting list of activities, also query parameters for filtering activities and map controller also now switches routes

--- Assets/MapController.cs
+++ Assets/MapController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mapbox.Unity.Map;
 using Mapbox.Utils;
+using System;
 
 public class MapController : MonoBehaviour {
 
@@ -18,9 +19,12 @@ public class MapController : MonoBehaviour {
 	private List<int> mapZoomLevels = new List<int> ();
 	private int currentMapIndex = 0;
 
+	public StravaSpheres stravaSpheres;
 
 	// Use this for initialization
 	void Start () {
+		GameObject go = GameObject.Find("Map");
+		stravaSpheres = (StravaSpheres) go.GetComponent(typeof(StravaSpheres));
 		targetRotation = transform.rotation;
 		AddListOfMaps ();
 		_map.Initialize (mapCoordinates [currentMapIndex], mapZoomLevels [currentMapIndex]);
@@ -32,7 +36,7 @@ public class MapController : MonoBehaviour {
 	}
 
 	private void AddListOfMaps () {
-		// San Fran (initial map)
+		// Dublin (initial map)
 		mapCoordinates.Add(new Vector2d(53.3441655,-6.2532845));
 		mapZoomLevels.Add (16);
 
@@ -57,10 +61,22 @@ public class MapController : MonoBehaviour {
 	public void NextMap () {
 		currentMapIndex = (currentMapIndex + 1) % mapCoordinates.Count;
 		_map.Initialize (mapCoordinates [currentMapIndex], mapZoomLevels [currentMapIndex]);
+		if (currentMapIndex == 0) {
+			stravaSpheres.swapRoute("darragh", "1437408506", 134);
+		}
+		else if (currentMapIndex == 1) {
+			stravaSpheres.swapRoute("tim", "1467628154", 993);
+		}
 	}
 
 	public void PreviousMap () {
 		currentMapIndex = (currentMapIndex - 1) % mapCoordinates.Count;
 		_map.Initialize (mapCoordinates [currentMapIndex], mapZoomLevels [currentMapIndex]);
+		if (currentMapIndex == 0) {
+			stravaSpheres.swapRoute("darragh", "1437408506", 134);
+		}
+		else if (currentMapIndex == 1) {
+			stravaSpheres.swapRoute("tim", "1467628154", 993);
+		}
 	}
 }
--- TASA_api/TASA_api.py
+++ TASA_api/TASA_api.py
@@ -1,45 +1,82 @@
 from stravalib.client import Client
-from flask import Flask, Response
+from flask import Flask, Response, request
 import json
+import config
 
 app = Flask(__name__)
 client = Client()
-client.access_token = "d7c39d6933831f833e61b52632e13bd7fd710de1" #d107
 
-# client.access_token = "55d1f207d59c98d061a2016ed77225d9ebb9a2f4" #dmc77
+client.access_token = config.CYCLE_TOKEN
 
-
-# client.access_token = "bf356fe1baf6b9dde0d49fa70e7f84d2a647fdff" # yas
 types = ['time', 'latlng', 'altitude', 'heartrate', 'temp', ]
-activity_id = 1437408506
-tim_activity_id = 1467628154
-yas_activity_id = 1440429292
 
+
+# d_activity_id = 1437408506
+# tim_activity_id = 1467628154
+# yas_activity_id = 1440429292
+
+def setUser(user):
+    if (user == 'tim'):
+        client.access_token = config.SKI_TOKEN
+    elif (user == 'darragh'):
+        client.access_token = config.DUBLIN_WALK_TOKEN
+    elif (user == 'yas'):
+        client.access_token = config.CYCLE_TOKEN
+
+
+# Return information about logged in athlete
 @app.route('/athlete')
 def get_athlete():
     athlete = client.get_athlete()
-    print(athlete)
-    return "ok"
+    return athlete
 
-@app.route('/activity')
-def get_activity():
+
+@app.route('/stream')
+def get_activities():
+    activity_id = request.get('id')
     activity = client.get_activity(activity_id)
     print(activity)
     return "ok"
 
-@app.route('/stream')
-def get_stream():
+
+@app.route('/activity_list')
+def get_activity_list():
+    user = request.args.get('user')
+    setUser(user)
+    activity_limit = int(request.args.get('limit'))
+    activity_request = client.get_activities(after="2010-01-01T00:00:00Z", limit=activity_limit)
+
+    activity_list = []
+
+    for activity in activity_request:
+        activity_list.append({"id": activity.id, "name": activity.name, "date": activity.start_date.isoformat()})
+
+    return json.dumps(activity_list)
+
+
+@app.route('/activity')
+def get_activity():
+    user = request.args.get('user')
+    setUser(user)
+    activity_id = request.args.get('id')
+
     streams = client.get_activity_streams(activity_id, types=types, resolution='medium')
     print(streams)
-    # latlng = []
+
     if 'latlng' in streams.keys():
         print(streams['latlng'].data)
         latlng = streams['latlng'].data
         print(len(latlng))
     return json.dumps(latlng)
 
+
 @app.route('/altitude')
 def get_altitude():
+    user = request.args.get('user')
+    setUser(user)
+
+    activity_id = request.get('id')
+
     streams = client.get_activity_streams(activity_id, types=types, resolution='medium')
     print(streams)
     if 'altitude' in streams.keys():
@@ -47,8 +84,6 @@ def get_altitude():
         print(streams['altitude'].data)
     return json.dumps(altitudes)
 
+
 if __name__ == '__main__':
-    # athlete = get_athlete()
-    # activity = get_activity()
-    # stream = get_stream()
     app.run(host="127.0.0.1", port=8010)
